95ef3cdc6fa346ab98bb01d41a5284f5addacf73
apollo1840 <zoucongyu1993@hotmail.com>
2023-03-21 15:38:54 +0100

    init

---
+++ active_learning_mnist.py
@@ -0,0 +1,117 @@
+"""
+
+This code is highly inspired by gtoubassi from github: https://github.com/gtoubassi/active-learning-mnist/blob/master/Active_Learning_with_MNIST.ipynb
+
+"""
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def mlp_classifier():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
+
+
+def eval_prioritization_strategy(data, model, prioritizer, verbose=True):
+    """
+
+    :param model:
+    :param prioritizer: lamdba: indices, predictions: sorted indices
+    :return:
+    """
+    query_steps = 40
+    query_size = 500
+
+    x_train, y_train, x_test, y_test = data
+
+    train_indices = range(60000)
+
+    # First subset of data
+    selected_indices = train_indices[0:query_size]
+    x_train_subset = x_train[selected_indices, ...]
+    y_train_subset = y_train[selected_indices, ...]
+
+    test_accuracies = []
+    for i in range(query_steps):
+        selected_indices = train_indices[0:query_size]
+
+        if i > 0:
+            x_train_subset = np.concatenate((x_train_subset, x_train[selected_indices, ...]))
+            y_train_subset = np.concatenate((y_train_subset, y_train[selected_indices, ...]))
+
+        model.fit(x_train_subset, y_train_subset, epochs=5, verbose=0)
+
+        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+        test_accuracies.append(accuracy)
+
+        if verbose:
+            print('Training data size of %d => accuracy %f' % (x_train_subset.shape[0], accuracy))
+
+        # prepare for the next step of the loop
+        rest_indices = train_indices[query_size:]
+        predictions = model.predict(x_train[rest_indices, ...])
+
+        train_indices = prioritizer(rest_indices, predictions)
+
+    return test_accuracies
+
+
+trivial_strategy = lambda indices, pred: indices
+
+
+def max_entropy_strategy(indices, predictions):
+    p = predictions * np.log(predictions)
+    p = -p.sum(axis=1)
+    p = list(zip(indices, p))
+    p.sort(reverse=True, key=lambda x: x[1])  # sort in descending order
+    return list(zip(*p))[0]
+
+
+def least_margin_strategy(indices, predictions):
+    # breaking tie
+    p = -np.sort(-predictions)  # sort in descending order
+    p = p[:, 0] - p[:, 1]
+    p = list(zip(indices, p))
+    p.sort(key=lambda x: x[1])  # sort in ascending order
+    return list(zip(*p))[0]
+
+
+def least_confidence_strategy(indices, predictions):
+    # variation ratio
+    max_logit = list(zip(indices, np.amax(predictions, axis=1)))
+    max_logit.sort(key=lambda x: x[1])  # sort in ascending order
+    return list(zip(*max_logit))[0]
+
+
+if __name__ == '__main__':
+
+    # prepare data
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    data = x_train, y_train, x_test, y_test
+
+    # prepare model
+    model = mlp_classifier()
+
+    # get results of different AL method
+    acc_baseline = eval_prioritization_strategy(data, model, trivial_strategy)
+    acc_entropy = eval_prioritization_strategy(data, model, max_entropy_strategy)
+    acc_bt = eval_prioritization_strategy(data, model, least_margin_strategy)
+    acc_vr = eval_prioritization_strategy(data, model, least_confidence_strategy)
+
+    # visualize the performance difference
+    plt.plot(acc_baseline, 'k', label='baseline')
+    plt.plot(acc_entropy, 'b', label='least confidence')
+    plt.plot(acc_bt, 'g', label='highest entropy')
+    plt.plot(acc_vr, 'r', label='least margin')
+    plt.legend()
