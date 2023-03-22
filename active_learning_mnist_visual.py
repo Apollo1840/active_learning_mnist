dea690c493a824dc283c46a9647fe2fd38d2c383
apollo1840 <zoucongyu1993@hotmail.com>
2023-03-22 17:16:25 +0100

    debug

--- active_learning_mnist_visual.py
+++ active_learning_mnist_visual.py
@@ -7,29 +7,34 @@ This code is highly inspired by gtoubassi from github: https://github.com/gtouba
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 from .model import mlp_classifier
+from .active_learning_mnist import (trivial_strategy,
+                                    max_entropy_strategy,
+                                    least_margin_strategy,
+                                    least_confidence_strategy)
 
-def eval_prioritization_strategy(data, model, prioritizer, verbose=True):
+
+def plot_prioritization_strategy(data, model, prioritizer):
     """
 
     :param model:
     :param prioritizer: lamdba: indices, predictions: sorted indices
     :return:
     """
-    query_steps = 40
+    query_steps = 20
     query_size = 500
 
     x_train, y_train, x_test, y_test = data
 
-    train_indices = range(60000)
+    train_indices = range(10000)
 
     # First subset of data
     selected_indices = train_indices[0:query_size]
     x_train_subset = x_train[selected_indices, ...]
     y_train_subset = y_train[selected_indices, ...]
 
-    test_accuracies = []
     for i in range(query_steps):
         selected_indices = train_indices[0:query_size]
 
@@ -37,13 +42,9 @@ def eval_prioritization_strategy(data, model, prioritizer, verbose=True):
             x_train_subset = np.concatenate((x_train_subset, x_train[selected_indices, ...]))
             y_train_subset = np.concatenate((y_train_subset, y_train[selected_indices, ...]))
 
+        plot_train_set(x_train, x_train_subset)
+
         model.fit(x_train_subset, y_train_subset, epochs=5, verbose=0)
-
-        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-        test_accuracies.append(accuracy)
-
-        if verbose:
-            print('Training data size of %d => accuracy %f' % (x_train_subset.shape[0], accuracy))
 
         # prepare for the next step of the loop
         rest_indices = train_indices[query_size:]
@@ -52,38 +53,33 @@ def eval_prioritization_strategy(data, model, prioritizer, verbose=True):
         train_indices = prioritizer(rest_indices, predictions)
 
     print("- finished -")
-    return test_accuracies
 
 
-trivial_strategy = lambda indices, pred: indices
+def plot_train_set(x_train, x_train_subset, x_train_dim2=None):
+    # Apply t-SNE to x_train only
+    tsne = TSNE(n_components=2, random_state=42)
 
+    x_concat = np.concatenate((x_train, x_train_subset), axis=0)
 
-def max_entropy_strategy(indices, predictions):
-    p = predictions * np.log(predictions)
-    p = -p.sum(axis=1)
-    p = list(zip(indices, p))
-    p.sort(reverse=True, key=lambda x: x[1])  # sort in descending order
-    return list(zip(*p))[0]
+    x_train_tsne = tsne.fit_transform(x_train)
+    x_concat_tsne = tsne.fit_transform(x_concat)
+    # Apply the trained t-SNE model to both datasets
+    x_train_subset_tsne = tsne.transform(x_train_subset)
 
+    # Create a scatter plot
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.scatter(x_train_tsne[:, 0], x_train_tsne[:, 1], c='b', s=20)
+    ax.scatter(x_train_subset_tsne[:, 0], x_train_subset_tsne[:, 1], c='orange', s=5)
 
-def least_margin_strategy(indices, predictions):
-    # breaking tie
-    p = -np.sort(-predictions)  # sort in descending order
-    p = p[:, 0] - p[:, 1]
-    p = list(zip(indices, p))
-    p.sort(key=lambda x: x[1])  # sort in ascending order
-    return list(zip(*p))[0]
+    # Set axis labels and title
+    ax.set_xlabel('t-SNE 1')
+    ax.set_ylabel('t-SNE 2')
+    ax.set_title('t-SNE Plot of x_train and x_train_subset')
+    plt.show()
 
-
-def least_confidence_strategy(indices, predictions):
-    # variation ratio
-    max_logit = list(zip(indices, np.amax(predictions, axis=1)))
-    max_logit.sort(key=lambda x: x[1])  # sort in ascending order
-    return list(zip(*max_logit))[0]
 
 
 if __name__ == '__main__':
-
     # prepare data
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -91,10 +87,10 @@ if __name__ == '__main__':
     data = x_train, y_train, x_test, y_test
 
     # get results of different AL method
-    acc_baseline = eval_prioritization_strategy(data, mlp_classifier(), trivial_strategy)
-    acc_entropy = eval_prioritization_strategy(data, mlp_classifier(), max_entropy_strategy)
-    acc_bt = eval_prioritization_strategy(data, mlp_classifier(), least_margin_strategy)
-    acc_vr = eval_prioritization_strategy(data, mlp_classifier(), least_confidence_strategy)
+    acc_baseline = plot_prioritization_strategy(data, mlp_classifier(), trivial_strategy)
+    acc_entropy = plot_prioritization_strategy(data, mlp_classifier(), max_entropy_strategy)
+    acc_bt = plot_prioritization_strategy(data, mlp_classifier(), least_margin_strategy)
+    acc_vr = plot_prioritization_strategy(data, mlp_classifier(), least_confidence_strategy)
 
     # visualize the performance difference
     plt.plot(acc_baseline, 'k', label='baseline')
