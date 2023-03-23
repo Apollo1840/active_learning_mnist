a63830b88fb3715e8569665b88a1313fe2898004
apollo1840 <zoucongyu1993@hotmail.com>
2023-03-23 19:58:57 +0100

    debug

--- active_learning_mnist_annotation.py
+++ active_learning_mnist_annotation.py
@@ -1,17 +1,21 @@
 import tensorflow as tf
 import threading
 import random
-from mnist_annotation import AnnotationTool
+from mnist_annotation import AnnotationTool, cnn
 
 
 class ActiveAnnotationTool(AnnotationTool):
-    display_tmp = "Current batch: {ind}/{s_q}, total annotations: {n_ann}\ncurrent model accuracy: {acc:.2f}"
+    display_tmp = "Current batch: {ind}/{q_s}, total annotations: {n_ann}\ncurrent model accuracy: {acc:.2f}"
 
     def __init__(self, data, query_strategy, query_size=16):
         super().__init__(data, query_size)
+
+        print(self.query_size)
+
         self.query_strategy = query_strategy
-        
+
         self.unlabeled_indices = list(range(len(self.images)))
+        self.annotated_indices = []
         random.shuffle(self.unlabeled_indices)
         self.curr_batch = self.unlabeled_indices[:self.query_size]
         self.next_batch = []
@@ -21,7 +25,9 @@ class ActiveAnnotationTool(AnnotationTool):
     def next_image(self):
         label = int(self.label_var.get())
         print(f"Image {self.index}: Label {label}")
+        print("curr_batch: {}, next_batch: {}".format(len(self.curr_batch), len(self.next_batch)))
 
+        self.annotated_indices.append(self.index)
         self.annotated_images.append(self.images[self.index])
         self.annotated_labels.append(label)
 
@@ -29,7 +35,7 @@ class ActiveAnnotationTool(AnnotationTool):
         n_ann = len(self.annotated_images)
         self.model_accuracy.set(self.display_tmp.format(ind=ind, q_s=self.query_size, n_ann=n_ann, acc=self.acc_curr))
 
-        if self.curr_batch:
+        if len(self.curr_batch) != 0:
             self.index = self.curr_batch.pop(0)
         elif not self.is_training and self.next_batch:
             self.curr_batch = self.next_batch
@@ -47,32 +53,16 @@ class ActiveAnnotationTool(AnnotationTool):
         if len(self.annotated_images) % self.query_size == 0 and not self.is_training:
             threading.Thread(target=self.train_model).start()
 
-        if not self.next_batch and not self.is_training:
+        if len(self.next_batch) == 0 and not self.is_training:
             threading.Thread(target=self.prepare_next_batch).start()
 
     def prepare_next_batch(self):
-        remaining_indices = [i for i in self.unlabeled_indices if i not in self.annotated_images]
+        remaining_indices = [i for i in self.unlabeled_indices if i not in self.annotated_indices]
         predictions = self.model.predict(self.images[remaining_indices])
         next_batch_indices = self.query_strategy(remaining_indices, predictions)[:self.query_size]
 
         # Update next_batch with the new batch
         self.next_batch = next_batch_indices
-
-
-def cnn():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(10, activation="softmax")
-    ])
-
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-    return model
 
 
 if __name__ == "__main__":
@@ -83,6 +73,6 @@ if __name__ == "__main__":
     data = x_train, y_train, x_test, y_test
 
     trivial_strategy = lambda indices, pred: indices
-    annotation_tool = AnnotationTool(data, trivial_strategy)
+    annotation_tool = ActiveAnnotationTool(data, trivial_strategy)
     annotation_tool.connect(cnn())
     annotation_tool.mainloop()
