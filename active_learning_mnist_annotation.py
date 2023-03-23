d0ee7cc502461724f1b19655aec2e58d04c45182
apollo1840 <zoucongyu1993@hotmail.com>
2023-03-23 19:08:36 +0100

    better display

--- active_learning_mnist_annotation.py
+++ active_learning_mnist_annotation.py
@@ -8,6 +8,8 @@ import threading
 
 
 class AnnotationTool(tk.Tk):
+    display_tmp = "Current batch: {ind}/16, total annotations: {n_annotations}\ncurrent model accuracy: {acc:.2f}"
+
     def __init__(self, data):
         super().__init__()
 
@@ -15,9 +17,11 @@ class AnnotationTool(tk.Tk):
         self.annotated_images = []
         self.annotated_labels = []
 
-        self.model = self.create_cnn_model()
+        self.model = None  # wait to be assigned
+
         self.model_accuracy = tk.StringVar()
-        self.model_accuracy.set("Model accuracy: N/A")
+        self.curr_accuracy = 0
+        self.model_accuracy.set(self.display_tmp.format(ind=0, n_annotations=0, acc=self.curr_accuracy))
 
         self.title("MNIST Annotation Tool")
         self.index = 0
@@ -43,20 +47,10 @@ class AnnotationTool(tk.Tk):
         self.protocol("WM_DELETE_WINDOW", self.on_close)
         self.show_image()
 
-    def create_cnn_model(self):
-        model = tf.keras.models.Sequential([
-            tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dense(10, activation="softmax")
-        ])
+        self.is_training = False
 
-        model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-        return model
+    def connect(self, model):
+        self.model = model
 
     def show_image(self):
         img = self.images[self.index]
@@ -68,20 +62,28 @@ class AnnotationTool(tk.Tk):
     def next_image(self):
         label = int(self.label_var.get())
         print(f"Image {self.index}: Label {label}")
+
         self.annotated_images.append(self.images[self.index])
         self.annotated_labels.append(label)
 
+        ind = len(self.annotated_images) % 16
+        n_annotations = len(self.annotated_images)
+        self.model_accuracy.set(self.display_tmp.format(ind=ind, n_annotations=n_annotations, acc=self.curr_accuracy))
+
         self.index += 1
+
         if self.index < len(self.images):
             self.label_var.set(str(self.labels[self.index]))
             self.show_image()
         else:
             self.destroy()
 
-        if len(self.annotated_images) % 16 == 0:
+        if len(self.annotated_images) % 16 == 0 and not self.is_training:
             threading.Thread(target=self.train_model).start()
 
     def train_model(self):
+        self.is_training = True
+
         self.model_accuracy.set("Model is fitting, please wait...")
         self.update()
 
@@ -89,12 +91,33 @@ class AnnotationTool(tk.Tk):
         y_train = np.array(self.annotated_labels)
 
         self.model.fit(x_train, y_train, epochs=5, verbose=0)
-        _, accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-        self.model_accuracy.set(f"Model accuracy: {accuracy:.2%}")
+        _, self.curr_accuracy = self.model.evaluate(self.x_test, self.y_test, verbose=0)
+
+        ind = len(self.annotated_images) % 16
+        n_annotations = len(self.annotated_images)
+        self.model_accuracy.set(self.display_tmp.format(ind=ind, n_annotations=n_annotations, acc=self.curr_accuracy))
+
+        self.is_training = False
 
     def on_close(self):
         print("Annotation tool closed.")
         self.destroy()
+
+
+def cnn():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(10, activation="softmax")
+    ])
+
+    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    return model
 
 
 if __name__ == "__main__":
@@ -105,4 +128,5 @@ if __name__ == "__main__":
     data = x_train, y_train, x_test, y_test
 
     annotation_tool = AnnotationTool(data)
+    annotation_tool.connect(cnn())
     annotation_tool.mainloop()
